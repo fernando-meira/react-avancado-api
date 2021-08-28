f2b0c462238a2044c5cf6e3ed171d67d3d608436
fernando-meira <thomwork@outlook.com>
2021-08-28 17:16:47 -0300

    feat: 🎸 Started project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '29b378abd54439eeebe98e58c23b65ba'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd769193d-26a3-44b4-9931-3f1b51ae7b09'
+};
